d9fc8dd6f883d18b662480b5260482a56a3e739d
Your Name <vemasani.pavani@gmail.com>
2026-03-26 12:34:13 +0530

    run github action runner through terraform

--- main.tf
+++ main.tf
@@ -1,21 +1,19 @@
 resource "aws_instance" "instance" {
-  for_each      = var.components
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.sg]
   tags = {
-    Name = each.key
+    Name = "github-runner"
   }
 }
 
 resource "null_resource" "provisioner" {
-  for_each      = var.components
   depends_on = [
     aws_instance.instance
   ]
   triggers = {
     timestamp = timestamp()
-    instance_id = aws_instance.instance[each.key].id
+    instance_id = aws_instance.instance.id
   }
 
   provisioner "remote-exec" {
@@ -23,12 +21,12 @@ resource "null_resource" "provisioner" {
       type     = "ssh"
       user     = "ec2-user"
       password = "DevOps321"
-      host     = aws_instance.instance[each.key].private_ip
+      host     = aws_instance.instance.private_ip
     }
     inline = [
       "sudo dnf install python3.11-pip -y",
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/pdevops87/github-actions-ci runner.yaml -e ORG=${each.key} -e TOKEN=${var.TOKEN} -e NAME=${var.NAME}"
+      "ansible-pull -i localhost, -U https://github.com/pdevops87/github-actions-ci runner.yaml -e URL=${var.URL} -e TOKEN=${var.TOKEN}"
     ]
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,36 +2,10 @@ variable "ami" {
   default="ami-0220d79f3f480ecf5"
 }
 variable "instance_type" {
-  default="t2.micro"
-}
-variable "zone_id"{
-  default="Z08520602FC482APPVUI7"
-}
-# variable "components"{
-#   default = ["frontend","catalogue","cart","users","mongodb"]
-# }
-variable "role" {
-  default = "test-role"
-}
-variable "policy"{
-  default = "test-policy"
-}
-variable "instance_profile"{
-  default = "instance_profile"
+  default="t3.small"
 }
 variable "env" {
   default = "dev"
 }
-variable "components" {
-  default = {
-    frontend    =   ""
-    catalogue   =   ""
-    cart        =   ""
-    user        =   ""
-    shipping    =   ""
-    payment     =   ""
-  }
-}
-
 variable "TOKEN"{}
-variable "NAME"{}
+variable "URL" {}
